b4cc60b168303cf0ae43dde464298b118d8c1cc6
Octavian Tuchila <octavian@dyn1220-211.wlan.ic.ac.uk>
2014-10-23 18:07:33 +0100

    Passing index page integration tests

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,22 +1,30 @@
 require 'rails_helper'
 
 feature 'Someone visits the home page' do
+
   scenario 'and signs up' do
     visit '/'
-    fill_in 'Email', with: '123@gmail.com'
-    fill_in 'Password', with: '1234567890'
-    fill_in 'Password confirmation', with: '1234567890'
-    click_button 'Sign up'
-
-    expect(page).to have_content 'Welcome to RadiCalc'
+    within 'div.registration' do
+      fill_in 'Email', with: 'octavian@gmail.com'
+      fill_in 'Password', with: '1234567890'
+      fill_in 'Password confirmation', with: '1234567890'
+      click_button 'Sign up'
+    end
+      expect(page).to have_content 'Welcome to RadiCalc'
   end
 
-  scenario 'and login' do
-    visit '/'
-    fill_in 'Email', with: '123@gmail.com'
-    fill_in 'Password', with: '1234567890'
-    click_button 'Sign up'
 
+  before do
+    User.create(:email => 'user@example.com', :password => 'password')
+  end
+
+  scenario 'and logins n' do
+    visit '/'
+    within 'div.session' do
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Sign in'
+    end
     expect(page).to have_content 'Welcome'
   end
 
