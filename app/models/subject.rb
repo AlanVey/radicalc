86b1a968b9430843711f51ae753a593542b44fb7
Octavian Tuchila <octavian@dyn1221-49.wlan.ic.ac.uk>
2014-10-23 14:21:11 +0100

    Added validations for the Subject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,6 @@
 class Subject < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :body
+  validates_presence_of :user_id
   belongs_to :user
 end
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Subject, :type => :model do
+
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:subject)).to be_valid
+  end
+
   it 'has a name' do
-    @subject = FactoryGirl.create(:subject, user_id:nil)
-    @subject.name should_not = nil
+    expect(FactoryGirl.build(:subject, name: nil)).not_to be_valid
+  end
+
+  it 'has a body' do
+    expect(FactoryGirl.build(:subject, body: nil)).not_to be_valid
   end
 
   it 'has a user id' do
-    @subject = FactoryGirl.create(:subject, user_id: nil)
-    @subject.name = ''
-    @subject.save
+    expect(FactoryGirl.build(:subject, user_id: nil)).not_to be_valid
   end
-
-  it 'can have no discription' do
-    #do nothing
-  end
-
 
 end
