1f9b901fbfb453963dc2209cfd250d8036d33d87
Alan <alan.vey12@imperial.ac.uk>
2014-11-04 16:09:37 +0000

    User redirect bugs fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, :controllers => {registrations: 'registrations', passwords: 'passwords', sessions: 'sessions'}
   
   root 'home#index'
+
   get 'dashboard/index', to: 'dashboard#index', as: 'dashboard'
 
 end
