803ec1326da441c6d746903db34af22b25269b02
Octavian Tuchila <octavian@dyn1217-65.wlan.ic.ac.uk>
2014-11-06 18:16:16 +0000

    Removed fillin in of user from tests

--- spec/features/hierarchy_spec.rb
+++ spec/features/hierarchy_spec.rb
@@ -7,7 +7,6 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N1'
     fill_in 'Body', with: 'B1'
-    fill_in 'User', with: 1
     click_button 'Create Subject'
     expect(Subject.all.count).to eq(1)
     expect(Subject.first.name).to eq('N1')
@@ -20,13 +19,11 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N1'
     fill_in 'Body', with: 'B1'
-    fill_in 'User', with: 1
     click_button 'Create Subject'
     click_link 'Back'
     click_link 'New Subject'
     fill_in 'Name', with: 'N2'
     fill_in 'Body', with: 'B2'
-    fill_in 'User', with: 'U2'
     click_button 'Create Subject'
     expect(Subject.all.count).to eq(2)
     expect(Subject.first.name).to eq('N1')
@@ -40,19 +37,16 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N1'
     fill_in 'Body', with: 'B1'
-    fill_in 'User', with: 1
     click_button 'Create Subject'
     click_link 'Back'
     click_link 'Show'
     click_link 'New Subject'
     fill_in 'Name', with: 'N2'
     fill_in 'Body', with: 'B2'
-    fill_in 'User', with: 'U2'
     click_button 'Create Subject'
     click_link 'New Subject'
     fill_in 'Name', with: 'N3'
     fill_in 'Body', with: 'B3'
-    fill_in 'User', with: 'U3'
     click_button 'Create Subject'
 
     expect(Subject.all.count).to eq(3)
@@ -70,7 +64,6 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N1'
     fill_in 'Body', with: 'B1'
-    fill_in 'User', with: 1
     click_button 'Create Subject'
     click_link 'Back'
 
@@ -78,7 +71,6 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N2'
     fill_in 'Body', with: 'B2'
-    fill_in 'User', with: 'U2'
     click_button 'Create Subject'
     click_link 'Back'
 
@@ -86,7 +78,6 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N3'
     fill_in 'Body', with: 'B3'
-    fill_in 'User', with: 'U3'
     click_button 'Create Subject'
 
     expect(Subject.all.count).to eq(3)
@@ -103,21 +94,18 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N1'
     fill_in 'Body', with: 'B1'
-    fill_in 'User', with: 1
     click_button 'Create Subject'
     click_link 'Back'
     click_link 'Show'
     click_link 'New Subject'
     fill_in 'Name', with: 'N2'
     fill_in 'Body', with: 'B2'
-    fill_in 'User', with: 'U2'
     click_button 'Create Subject'
 
     expect(page).to have_content 'Parent:'
     click_link 'N1'
     expect(page).to have_content 'N1'
     expect(page).to have_content 'B1'
-    expect(page).to have_content 1
   end
 
 
@@ -126,14 +114,12 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N1'
     fill_in 'Body', with: 'B1'
-    fill_in 'User', with: 1
     click_button 'Create Subject'
     click_link 'Back'
     click_link 'Show'
     click_link 'New Subject'
     fill_in 'Name', with: 'N2'
     fill_in 'Body', with: 'B2'
-    fill_in 'User', with: 'U2'
     click_button 'Create Subject'
 
     click_link 'N1'
@@ -141,7 +127,6 @@ feature 'someone visits the dashboard' do
     click_link 'N2'
     expect(page).to have_content 'N2'
     expect(page).to have_content 'B2'
-    expect(page).to have_content 2
   end
 
   scenario 'and creates a parent and a child, checks that only the parent\'s page is accessible from the dashbord' do
@@ -149,21 +134,18 @@ feature 'someone visits the dashboard' do
     click_link 'New Subject'
     fill_in 'Name', with: 'N1'
     fill_in 'Body', with: 'B1'
-    fill_in 'User', with: 1
     click_button 'Create Subject'
     click_link 'Back'
     click_link 'Show'
     click_link 'New Subject'
     fill_in 'Name', with: 'N2'
     fill_in 'Body', with: 'B2'
-    fill_in 'User', with: 'U2'
     click_button 'Create Subject'
 
     click_link 'N1'
     click_link 'Back'
     expect(page).not_to have_content 'N2'
     expect(page).not_to have_content 'B2'
-    expect(page).not_to have_content 2
   end
 
 end
