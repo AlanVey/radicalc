f84c1b249a730d06b040cb86eb7e365e192038d2
Alan <alan.vey12@imperial.ac.uk>
2014-11-11 17:41:28 +0000

    Heroku bug fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,6 @@ CarrierWave.configure do |config|
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET'],
-      :region                => ENV['S3_REGION']
     }
    
     config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
