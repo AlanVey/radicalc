5709c1265dd9bc9125be2c308e7c9f621393a640
Alan <alan.vey12@imperial.ac.uk>
2014-11-11 17:11:28 +0000

    Bug fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
 
-  if Rails.env.deployment?
+  if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
@@ -12,19 +12,8 @@ CarrierWave.configure do |config|
     config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
    
     config.fog_directory    = ENV['S3_BUCKET_NAME']
-    config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
     config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
   end
 end
 
-=begin
-
-heroku config:add 
-
-S3_KEY         = AKIAI2GFGIII6MDPRG6Q 
-S3_SECRET      = AAJQxCgE2LtS70uDzc5AKwtQQVA+wBvC072qj5IK
-S3_REGION      = Ireland 
-S3_ASSET_URL   = http://assets.example.com/ 
-S3_BUCKET_NAME = radicalc/profile_pics
-
-=end
+# heroku config:add S3_KEY=AKIAI2GFGIII6MDPRG6Q S3_SECRET=AAJQxCgE2LtS70uDzc5AKwtQQVA+wBvC072qj5IK S3_REGION=Ireland S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=radicalc/profile_pics
