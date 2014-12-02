dfcdb7aaf0dd46068831602d8a3f00cc2fc23ada
Octavian Tuchila <octavian@dyn1221-142.wlan.ic.ac.uk>
2014-12-02 16:33:43 +0000

    Adapted merge so that all tests pass

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -22,7 +22,7 @@ feature 'Someone visits the dashboard' do
 
     visit "/subjects/#{@subject.id}"
     expect(page).to have_content 'Name:'
-    expect(page).to have_content 'Description:'
+    expect(page).to have_content 'Body:'
     expect(page).to have_content 'User:'
   end
 
