7ada7cfc53e68faad842d322d3b8e84bc3789c9f
Alan <alan.vey12@imperial.ac.uk>
2015-01-09 21:51:22 +0000

    Subject pic bug

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,6 @@
 class Subject < ActiveRecord::Base
   validates_presence_of :name, :body, :user_id
-  mount_uploader :icon, ProfilePicUploader
+  mount_uploader :icon, SubjectPicUploader
 
   resourcify
   include Authority::Abilities
